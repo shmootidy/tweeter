f56a6de46c89f4483b39070ea011ff906bedb3bc
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-22 21:50:46 +0000

    Refactor with ternary operator.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
 
-
   function eventPrinter () {
     const maxCharCount = 140;
-    let charCount = maxCharCount - $(this).val().length;
+    const charCount = maxCharCount - $(this).val().length;
     const counter = $(this).siblings('.counter');
     counter.text(charCount);
-    if (charCount < 0){
-      counter.css('color', 'red');
-    } else {
-      counter.css('color', 'black');
-    }
+    const countColor = (charCount < 0) ? 'red' : 'black';
+    counter.css('color', `${countColor}`);
   }
 
-
-  $('.new-tweet form textarea').on('input', eventPrinter); // using this because MDN doc suggests it'll work for the task at hand
+  $('.new-tweet form textarea').on('input', eventPrinter);
 });
-
-// https://www.tutorialrepublic.com/codelab.php?topic=faq&file=jquery-get-value-from-textarea
