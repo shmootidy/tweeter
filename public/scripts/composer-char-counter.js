ec9e202b6ef7c25d7faf0e5c4ecfbefaef835470
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-22 19:34:13 +0000

    Added a bunch of handlers to experiment.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  console.log("ok");
+  function eventPrinter (event) {
+    console.log(event);
+  }
+  $('.new-tweet').on('click', eventPrinter);
+  $('.new-tweet').on('change', eventPrinter);
+  $('.new-tweet').on('keyup', eventPrinter);
+  $('.new-tweet').on('keydown', eventPrinter);
+  $('.new-tweet').on('blur', eventPrinter);
+  $('.new-tweet').on('keypress', eventPrinter);
+  $('.new-tweet').on('input', eventPrinter);
 });
