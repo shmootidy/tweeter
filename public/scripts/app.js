46a86d671c7967a3a01e99116628ef1b9c53d2b1
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-23 20:51:43 +0000

    Use AJAX to fetch data from the server. Create loadTweets function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,7 +25,7 @@ $(function() {
       .append($('<span>').addClass('icon').text('♥'))));
 
     return articleElement;
-  }
+  } // end of function declaration
 
   function renderTweets(arrOfTweetObjs) {
     for (const tweetObj of arrOfTweetObjs){
@@ -34,63 +34,30 @@ $(function() {
     }
   }
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-  renderTweets(data);
+  const allTwits = $('#tweets-container');
+
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    }).done(function(tweets){
+      allTwits.empty();
+      renderTweets(tweets);
+    });
+  }
+
+  loadTweets();
 
   $('#submit-twit').on('submit', function(e) {
     e.preventDefault();
     const $twit = $(this).serialize();
+    console.log($twit);
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: $twit,
     }).done(function() {
-      console.log("CAW!");
+      loadTweets();
     });
   });
 
