08bc93fd882db7c132c221b1208808585eac4dba
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-24 01:22:03 +0000

    Refactor into a getTweets function.

--- mongo_example.js
+++ mongo_example.js
@@ -12,10 +12,22 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // connection to `test-tweets db` starts here
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  db.collection('tweets').find().toArray((err, results) => {
+  function getTweets(callback) {
+    db.collection('tweets').find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  getTweets((err, tweets) => {
     if (err) throw err;
 
-    console.log('results array: ', results);
+    console.log('Logging each tweet:');
+    for(let tweet of tweets) {
+      console.log(tweet);
+    }
 
     db.close();
   });
