dab377dac4f7af4fbfd0d512a98539a16ba27870
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-24 19:57:09 +0000

    Remove in-memory db and connect to Mongo, passing the Mongo db to datahelpers once connected.

--- server/index.js
+++ server/index.js
@@ -11,19 +11,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // remove the in-memory db
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
+
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URI = 'mongodb://localhost:27017/tweeter';
 
 // instead, connect to mongo
 // once connected, pass mongo db to data-helpers
-const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  // connection to `test-tweets db` starts here
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
 });
