45f5a1750b2b50b2ee13bc0b806396f774d9ca8c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-25 00:21:00 +0000

    More airbnb.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,21 +1,23 @@
-"use strict";
+/* eslint strict: 'off' */
+/* eslint func-names: 'off' */
+/* eslint object-shorthand: 'off' */
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+
+'use strict';
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
+    saveTweet: function (newTweet, callback) {
       db.collection('tweets').insertOne(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
+    getTweets: function (callback) {
       db.collection('tweets').find().toArray(callback);
-    }
+    },
 
   };
-}
+};
